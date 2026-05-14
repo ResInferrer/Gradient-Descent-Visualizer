b6201730661ca0e9b51e2b21ff83d778cac0e9b8
KotingGG <jekalavrovih@gmail.com>
2026-05-14 22:04:33 +0300

    feat: optimizers cpp core

--- core/src/optimizers/Optimizers.cpp
+++ core/src/optimizers/Optimizers.cpp
@@ -1,4 +1,5 @@
 #include "gdv/optimizers/Optimizers.h"
+#include <cmath>
 
 namespace gdv::optimizers {
 
@@ -8,11 +9,23 @@ Optimizers::Optimizers()
 }
 
 double Optimizers::L1_Norm(const std::vector<double>& omegas) {
+    double sum = 0.0;
 
+    for (double val : omegas) {
+        sum += std::abs(val);
+    }
+
+    return sum;
 }
 
 double Optimizers::L2_Norm(const std::vector<double>& omegas) {
+    double sum_sq = 0.0;
 
+    for (double val : omegas) {
+        sum_sq += val * val;
+    }
+
+    return std::sqrt(sum_sq);
 }
 
 }  // namespace gdv::optimizers
